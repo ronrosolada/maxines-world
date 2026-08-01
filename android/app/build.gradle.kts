770738eb560522f6afeff0f917ae9d4486c2c41d
Ron Rosolada <ron@maxinesworld.dev>
2026-08-01 21:26:17 +0800

    chore(release): bump versionCode 19, versionName 0.19.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.maxinesworld.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.10.0"
+        versionCode = 19
+        versionName = "0.19.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
