cea9739b635e29e10e10d5ccd2358711f9d64408
Ron Rosolada <ron@maxinesworld.dev>
2026-08-10 00:30:31 +0800

    release: bump app to v0.27.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.maxinesworld.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 27
-        versionName = "0.26.0"
+        versionCode = 28
+        versionName = "0.27.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
